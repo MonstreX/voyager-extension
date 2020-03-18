7f12fcf26b442b5f461b653ea9e55c27d25108f8
monstrex <fido6080net@gmail.com>
2020-03-18 18:02:44 +0600

    Config fixes

--- publishable/config/voyager-extension.php
+++ publishable/config/voyager-extension.php
@@ -2,4 +2,22 @@
 
 return [
 
+    /*
+    |
+    | CLone Record parameters
+    |
+    */
+    'clone_record' => [
+        'enabled' => true,
+        'reset_types' => ['image', 'multiple_images','file'],
+        'suffix_fields' => ['title','name','slug'],
+    ],
+    /*
+    |
+    | Here you can enable or disable the custom path generator for medialibrary images
+    | at MonstreX\VoyagerExtension\Generators\MediaLibraryPathGenerator
+    |
+    */
+
+    'use_media_path_generator' => true,
 ];
