f93941c1abee66b65975f4123f6b0689a6e3cdce
monstrex <fido6080net@gmail.com>
2020-03-25 11:38:10 +0600

    The alpha version release v0.7

---
+++ publishable/lang/ru/bread.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+    'adv_image' => [
+        'file'             => 'Имя файла:',
+        'title'            => 'Атрибут Title',
+        'alt'              => 'Атрибут Alt',
+        'delete'           => 'Удалить',
+        'not_set_title'    => '...',
+        'title_placeholder' => 'Введите TITLE атрибут для изображения',
+        'alt_placeholder'   => 'Введите ALT аттрибут для изображения',
+    ],
+
+    'adv_media_files' => [
+        'remove_selected'      => 'Удалить выбранные файлы',
+        'unmark_selected'      => 'Отменить выбор файлов',
+        'select_all'           => 'Выбрать все файлы',
+        'dialog_remove_images' => ' Изображение',
+        'dialog_remove_files'  => ' Медиа файлы',
+    ],
+
+    'adv_page_layout' => [
+        'remove_section_title'     => 'Удалить секцию',
+        'add_field_section'        => 'Добавить поле контента',
+        'add_block_section'        => 'Добавить блок (виджет)',
+        'add_form_section'         => 'Добавить форму',
+        'available_content_fields' => 'Доступные поля контента',
+        'available_blocks'         => 'Доступные блоки (виджеты)',
+        'available_forms'          => 'Доступный формы',
+        'field_title'              => 'Контент:',
+        'block_title'              => 'Блок:',
+        'form_title'               => 'Форма:',
+    ],
+
+    'media_removed'          => 'Удалено...',
+    'media_sorted'           => 'Отсортировано...',
+    'media_updated'          => 'Обновлено...',
+
+    'dialog_remove_title'     => 'Вы уверены?',
+    'dialog_remove_message'   => 'Вы собираетесь удалить: ',
+    'dialog_button_save'      => 'Сохранить',
+    'dialog_button_remove'    => 'Удалить',
+    'dialog_button_cancel'    => 'Отмена',
+    'dialog_button_change'    => 'Заменить',
+    'dialog_button_ok'        => 'OK',
+    'dialog_clone_message'    => 'Вы собираетесь клонировать выбранную запись.',
+    'dialog_clone_yes_button' => 'Клонировать',
+    'dialog_cant_delete_with_children_title' => 'Элемент является родительским',
+    'dialog_cant_delete_with_children' => 'Вы не можете удалить элемент, являющийся родительским для других элементов.',
+
+    'error_removing_media'    => 'Ошибка удаления медиа файла...',
+    'error_sorting_media'     => 'Ошибка сортировки медиа файлов...',
+    'error_saving_media'      => 'Ошибка сохранения медиа файла...',
+    'error_translation_loading' => 'Ошибка загрузки переводов',
+
+    'clone_action' => 'Клонировать',
+    'record_updated'          => 'Обновлено...',
+
+    'tab_main_title'          => 'Контент',
+
+];
