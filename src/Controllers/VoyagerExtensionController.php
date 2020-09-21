03656090511addbeb353502b8a73beb4828a637c
monstrex <fido6080net@gmail.com>
2020-09-21 23:32:39 +0600

    MediaLibrary 8.*, Laravel 8.*

--- src/Controllers/VoyagerExtensionController.php
+++ src/Controllers/VoyagerExtensionController.php
@@ -116,11 +116,6 @@ class VoyagerExtensionController extends BaseController
 
             // Save OLD Properties
             $old_file = $data->getMedia($field)->where('id', $media_file_id)->first();
-
-
-            //dd($old_file);
-
-
             $old_properties = [];
             $old_properties['title'] = $old_file->getCustomProperty('title');
             $old_properties['alt'] = $old_file->getCustomProperty('alt');
