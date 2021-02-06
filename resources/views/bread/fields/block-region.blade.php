bf8d0f6f1c81b5258392b7eb93c02f13cc1455dd
monstrex <fido6080net@gmail.com>
2021-02-06 12:36:03 +0600

    block-region.blade fix

--- resources/views/bread/fields/block-region.blade.php
+++ resources/views/bread/fields/block-region.blade.php
@@ -1,9 +1,11 @@
 @if(!isset($edit) && isset($data->positionId->color))
-<span class="badge badge-md region-block-badge" style="background-color: {{ $data->positionId->color }};">
-    @if($row->type == 'relationship')
-        @include('voyager::formfields.relationship', ['view' => 'browse','options' => $row->details])
-    @else
-        {{ $data->{$row->field} }}
+    @if ($row->type == 'relationship' || !empty($data->{$row->field}))
+        <span class="badge badge-md region-block-badge" style="background-color: {{ $data->positionId->color }};">
+            @if ($row->type == 'relationship')
+                @include('voyager::formfields.relationship', ['view' => 'browse','options' => $row->details])
+            @else
+                {{ $data->{$row->field} }}
+            @endif
+        </span>
     @endif
-</span>
 @endif
